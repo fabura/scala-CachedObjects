18a8eb9d0ac51142acd4e0129bcef7b658e1cad3
Bulat Fattahov <bulat.fattahov@inn.ru>
2013-05-21 01:14:16 +0400

    * Good work in concurrent environment.

--- src/scala/CachedObject.scala
+++ src/scala/CachedObject.scala
@@ -1,7 +1,8 @@
 package scala
 
-import java.util.concurrent.{ TimeUnit, Executors}
+import java.util.concurrent.{TimeUnit, Executors}
 import java.util.concurrent.atomic.AtomicBoolean
+import scala.collection.mutable
 
 /** Created by bulat.fattahov 2013
   * Uses for caching objects and refreshing cache by request.
@@ -19,30 +20,49 @@ class CachedObject[T](key: AnyRef, f: => T) extends (() => T) {
   private var value: Option[T] = None
 
   @volatile
-  private var reloadNeeded: Boolean = false
+  private var reloadNeeded: Boolean = true
 
   private final val loading: AtomicBoolean = new AtomicBoolean(false)
 
   CachedObject.addObserver(key, this)
 
   def apply(): T = {
-    if (value.isEmpty) { synchronized { if (value.isEmpty) { value = Some(f)}}}
+    if (value.isEmpty) {
+      synchronized {
+        if (value.isEmpty) {
+          load()
+        }
+      }
+    }
 
     if (reloadNeeded) {
-      if (loading.compareAndSet(false, true)) {
-        try {
-          value = Some(f)
-        }
-        finally {
-          loading.set(false)
-          reloadNeeded = false
-        }
+      if (loading.compareAndSet(false, true) || CachedObject.isLoadingThread(key)) {
+        load()
       }
     }
     value.get
   }
 
-  protected def invalidate() { reloadNeeded = true }
+
+  def load() {
+    synchronized {
+      if (reloadNeeded) {
+        try {
+          val thisThreadStartUpdateHere = CachedObject.trySetThreadLoading(key)
+          value = Some(f)
+          reloadNeeded = false
+          if (thisThreadStartUpdateHere) CachedObject.unsetThreadLoading(key)
+        }
+        finally {
+          loading.set(false)
+        }
+      }
+    }
+  }
+
+  protected def invalidate() {
+    reloadNeeded = true
+  }
 }
 
 object CachedObject {
@@ -54,7 +74,9 @@ object CachedObject {
   }
 
   def removeObserver(key: AnyRef, observer: CachedObject[_]) {
-    observers = observers.get(key).map(_.filterNot(_ == observer)).map {lst: List[CachedObject[_]] => observers + (key -> lst)}.getOrElse(observers)
+    observers = observers.get(key).map(_.filterNot(_ == observer)).map {
+      lst: List[CachedObject[_]] => observers + (key -> lst)
+    }.getOrElse(observers)
   }
 
   def removeKey(key: AnyRef) {
@@ -62,7 +84,30 @@ object CachedObject {
   }
 
   def refresh(key: AnyRef) {
-    observers(key).foreach(_.invalidate())
+    key.synchronized {
+      observers(key).foreach(_.invalidate())
+    }
+  }
+
+  private val threadLoading = new ThreadLocal[mutable.WeakHashMap[AnyRef, Boolean]] {
+    override def initialValue(): mutable.WeakHashMap[AnyRef, Boolean] = mutable.WeakHashMap.empty
+  }
+
+  protected def trySetThreadLoading(key: AnyRef): Boolean = {
+    if (!isLoadingThread(key)) {
+      println(this + "loading in " + Thread.currentThread())
+      threadLoading.get().put(key, true)
+      true
+    }
+    else false
+  }
+
+  protected def unsetThreadLoading(key: AnyRef) {
+    threadLoading.get().put(key, false)
+  }
+
+  protected def isLoadingThread(key: AnyRef): Boolean = {
+    threadLoading.get.get(key).getOrElse(false)
   }
 }
 
@@ -71,29 +116,44 @@ object CachedObjectTest {
 
     var i = 0
     def func = {
-      println("Sleep at "+ Thread.currentThread())
-      Thread.sleep(1000)
-      println("Wake up at "+ Thread.currentThread())
+      println("Sleep in first at "+ Thread.currentThread())
+      Thread.sleep(5000)
+      println("Wake up in first at "+ Thread.currentThread())
       i += 1
+      println("I value set to " + i )
       i
     }
 
     val cachedObject = new CachedObject(CachedObjectTest.getClass, func)
 
+    var j = 0
+    def func2 = {
+      println("Sleep in second at "+ Thread.currentThread())
+      Thread.sleep(5000)
+      println("Wake up in second at "+ Thread.currentThread())
+      j = cachedObject() + 1
+      println("J value set to " + j )
+      j
+    }
+
+    val cachedObject2 = new CachedObject(CachedObjectTest.getClass,func2)
+
     Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(new Runnable {
-      def run() { CachedObject.refresh(CachedObjectTest.getClass) }
-    }, 0, 1, TimeUnit.SECONDS)
+      def run() {
+        CachedObject.refresh(CachedObjectTest.getClass)
+      }
+    }, 0, 10, TimeUnit.SECONDS)
 
     Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Runnable {
       def run() {
-        println("Thread: " + Thread.currentThread() + " value: " + cachedObject())
+        println("Thread: " + Thread.currentThread() + " value: " + cachedObject2())
       }
-    }, 0, 100, TimeUnit.MILLISECONDS)
+    }, 0, 870, TimeUnit.MILLISECONDS)
 
     Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Runnable {
       def run() {
-        println("Thread: " + Thread.currentThread() + " value: " + cachedObject())
+        println("Thread: " + Thread.currentThread() + " value: " + cachedObject2())
       }
-    }, 0, 100, TimeUnit.MILLISECONDS)
+    }, 0, 123, TimeUnit.MILLISECONDS)
   }
 }
